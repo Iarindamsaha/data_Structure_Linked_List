63067fd0e1fe70cd7841d1bafebf12a2051b7f96
Arindam Saha <asaha15071998@gmail.com>
2023-02-14 22:57:07 +0530

    Creating a Simple Linked List

--- src/LinkedListProblems/LinkedProblems.java
+++ src/LinkedListProblems/LinkedProblems.java
@@ -1,4 +1,68 @@
 package LinkedListProblems;
 
-public class LinkedProblems {
+public class LinkedProblems<Integer> {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedProblems() {
+
+        this.size = 0;
+    }
+
+    public void addFirst(Integer val){
+
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if(tail == null){
+
+            tail =head;
+
+        }
+
+        size++;
+    }
+
+
+
+    public void displayAll(){
+
+        Node temp;
+        temp = head;
+        if (temp == null){
+            System.out.println("The List is Empty");
+        }
+        else {
+            while(temp != null){
+
+                System.out.print(temp.value + " ---> ");
+                temp = temp.next;
+            }
+            System.out.println("End of the List");
+        }
+    }
+
+
+
+
+
+
+
+    private class Node<Integer>{
+        private Integer value;
+        private Node next;
+
+        public Node(Integer value){
+            this.value = value;
+        }
+
+        public Node(Integer value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
 }
--- src/LinkedListProblems/Main.java
+++ src/LinkedListProblems/Main.java
@@ -2,6 +2,14 @@ package LinkedListProblems;
 
 public class Main {
     public static void main(String[] args) {
+
+        LinkedProblems<Integer> list = new LinkedProblems<>();
         System.out.println("---Welcome To Linked List Problems---");
+        System.out.println("---UC1---");
+        list.addFirst(56);
+        list.addFirst(30);
+        list.addFirst(70);
+        list.displayAll();
+
     }
 }
